d419ede411bbd08cabb9c1b60c6b4a6d8ad543bc
quenton2019 <15180172690@163.com>
2019-10-21 11:29:09 +0800

    modify lib sports

--- src/lib/sportsData.js
+++ src/lib/sportsData.js
@@ -21,17 +21,6 @@ const goodsData = {
 			resultSD: 50,
 			resultPB: 4,
 			startTime: '2019-10-20'
-		},
-		{
-			seq: 2,
-			targetFWC: 50,
-			resultFWC: 1,
-			targetYWQZ: 50,
-			resultYWQZ: 50,
-			targetSD: 50,
-			resultSD: 50,
-			resultPB: 4,
-			startTime: '2019-10-20'
 		}
   ]
 }
